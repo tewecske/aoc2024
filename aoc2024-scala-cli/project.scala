943fc35db2fe3a7961f66d1d2dd01a4bd493a41f
tewe <leventewe@gmail.com>
2024-12-12 11:29:22 +0100

    Scala CLI project setup

---
+++ aoc2024-scala-cli/project.scala
@@ -0,0 +1,6 @@
+//> using toolkit 0.6.0
+//> using option -feature -deprecation
+
+//> using dep "org.scala-lang.modules::scala-parallel-collections:1.0.4"
+//> using dep "com.lihaoyi::os-lib::0.11.3"
+
